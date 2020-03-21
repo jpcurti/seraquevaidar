9d876fad2f77ac62fe4a722ba039ab1e29041b7f
Vinícius Mazzola <vinimaz@gmail.com>
2020-03-21 20:43:57 +0100

    Downgrade to Angular 8

---
+++ sera-que-o-papel-vai-dar/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'sera-que-o-papel-vai-dar'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('sera-que-o-papel-vai-dar');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('sera-que-o-papel-vai-dar app is running!');
+  });
+});
---
+++ sera-que-o-papel-vai-dar/src/app/main/main.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  numPeople: number = 1;
+  sheetsPerUse: number = 20;
+  usesPerDay: number = 2;
+  daysSurviving: number = 0;
+  rolls: number = 16;
+  sheetsPerRoll: number = 200;
+  percentageSurviving: number = 0;
+  quarentineDays: number =14;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.calculateDays();
+  }
+
+  calculateDays(){
+    let totalUsedSheetsPerDay = (this.sheetsPerUse * this.usesPerDay * this.numPeople);
+    let totalAvailableSheets = (this.rolls * this.sheetsPerRoll);
+    this.daysSurviving = Math.floor(totalAvailableSheets / totalUsedSheetsPerDay);
+    this.percentageSurviving = Math.floor((this.daysSurviving/this.quarentineDays)*100);
+  }
+
+}
