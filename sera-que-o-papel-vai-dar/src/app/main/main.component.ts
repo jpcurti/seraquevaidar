b1c72a83ae7fd44008ac02d370954e73757fd70e
João Pedro Curti <joaocurti@hotmail.com>
2020-03-26 18:00:13 +0100

    changed sheets per roll to roll length

--- sera-que-o-papel-vai-dar/src/app/main/main.component.ts
+++ sera-que-o-papel-vai-dar/src/app/main/main.component.ts
@@ -16,6 +16,8 @@ export class MainComponent implements OnInit {
   percentageSurviving: number = 0;
   quarentineDays: number = 14;
   showAdvancedOptions: boolean = false;
+  lengthRoll: number= 30;
+  lengthSheet: number= 13;
   resultHashtagMessage: string = '#seraqueopapelvaidar?';
   descricaoCorona: string = "'Coronavírus é uma família de vírus que causam infecções respiratórias. O novo agente do coronavírus foi descoberto em 31/12/19 após casos registrados na China. Provoca a doença chamada de coronavírus (COVID-19).'+/n+'Os primeiros coronavírus humanos foram isolados pela primeira vez em 1937. No entanto, foi em 1965 que o vírus foi descrito como coronavírus, em decorrência do perfil na microscopia, parecendo uma coroa.'+/n+'A maioria das pessoas se infecta com os coronavírus comuns ao longo da vida, sendo as crianças pequenas mais propensas a se infectarem com o tipo mais comum do vírus. Os coronavírus mais comuns que infectam humanos são o alpha coronavírus 229E e NL63 e beta coronavírus OC43, HKU1...'";
 
@@ -26,8 +28,8 @@ export class MainComponent implements OnInit {
   }
 
   calculateDays(){
-    let totalUsedSheetsPerDay = (this.sheetsPerUse * this.usesPerDay * this.numPeople);
-    let totalAvailableSheets = (this.rolls * this.sheetsPerRoll);
+    let totalUsedSheetsPerDay = (this.sheetsPerUse * this.usesPerDay * this.numPeople * this.lengthSheet);
+    let totalAvailableSheets = (this.rolls * this.lengthRoll * 100);
     this.daysSurviving = Math.floor(totalAvailableSheets / totalUsedSheetsPerDay);
     this.percentageSurviving = Math.floor((this.daysSurviving/this.quarentineDays)*100);
     switch(true) { 
@@ -36,7 +38,7 @@ export class MainComponent implements OnInit {
          break; 
       } 
       case (this.percentageSurviving<100): { 
-        this.resultHashtagMessage= '#horadeeconomizar #seraqueopapelvaidar'; 
+        this.resultHashtagMessage= '#semdesperdicio #seraqueopapelvaidar'; 
          break; 
       } 
       case (this.percentageSurviving<150): { 
