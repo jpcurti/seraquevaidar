4df44a04ac9cb958e997c46fca66747d17d0b966
João Pedro Curti <joaocurti@hotmail.com>
2020-03-26 17:00:26 +0100

    added contact mail and covid description

--- sera-que-o-papel-vai-dar/src/app/main/main.component.ts
+++ sera-que-o-papel-vai-dar/src/app/main/main.component.ts
@@ -17,6 +17,7 @@ export class MainComponent implements OnInit {
   quarentineDays: number = 14;
   showAdvancedOptions: boolean = false;
   resultHashtagMessage: string = '#seraqueopapelvaidar?';
+  descricaoCorona: string = "'Coronavírus é uma família de vírus que causam infecções respiratórias. O novo agente do coronavírus foi descoberto em 31/12/19 após casos registrados na China. Provoca a doença chamada de coronavírus (COVID-19).'+/n+'Os primeiros coronavírus humanos foram isolados pela primeira vez em 1937. No entanto, foi em 1965 que o vírus foi descrito como coronavírus, em decorrência do perfil na microscopia, parecendo uma coroa.'+/n+'A maioria das pessoas se infecta com os coronavírus comuns ao longo da vida, sendo as crianças pequenas mais propensas a se infectarem com o tipo mais comum do vírus. Os coronavírus mais comuns que infectam humanos são o alpha coronavírus 229E e NL63 e beta coronavírus OC43, HKU1...'";
 
   constructor() { }
 
